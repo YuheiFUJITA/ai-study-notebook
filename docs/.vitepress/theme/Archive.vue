65b4661f83764459dcc3e0cb86433fbf75fe4a2a
Yuhei FUJITA <contact@fujita.dev>
2023-12-03 09:30:59 +0000

    feat: アーカイブ処理

---
+++ docs/.vitepress/theme/Archive.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="archive">
+    このノートブックはアーカイブされ、既存の記事は<a
+      href="https://github.com/YuheiFUJITA/internal-knowledge-base"
+      >YuheiFUJITA/internal-knowledge-base</a
+    >に移植されました。
+  </div>
+</template>
+
+<style scoped>
+.archive {
+  margin-bottom: 2rem;
+  padding: 0.5rem 0.85rem;
+  border: 1px solid;
+  border-radius: 4px;
+  color: #d97706;
+}
+
+a {
+  text-decoration: underline;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,12 @@
+import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
-import MyLayout from "./MyLayout.vue";
+import Archive from "./Archive.vue";
 
 export default {
   ...DefaultTheme,
-  Layout: MyLayout,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "doc-before": () => h(Archive),
+    });
+  },
 };
